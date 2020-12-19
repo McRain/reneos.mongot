2a7bd0b9f95f355b39bbc64a4735a55202b96bfe
McRain <8288917+McRain@users.noreply.github.com>
2020-12-19 22:41:39 +0400

    Init v1.0.11

---
+++ dbconn.js
@@ -0,0 +1,64 @@
+import Mongo from "mongodb"
+
+const _conns = {}
+
+/**
+ * 
+ */
+class DataConnection {
+	static GetConnection(key) {
+		return _conns[key]
+	}
+
+	/**
+	 * 
+	 * 
+	 */
+	constructor() {		
+		this._conn = null
+		this._db = null
+		return new Proxy(this, {
+			get(target, prop) {
+				if (target[prop] === undefined)
+					target[prop] = target._db.collection(prop.toLowerCase())
+				return target[prop]
+			}
+		})
+	}
+
+	/**
+	 * 
+	 * @param {Object} options : {url:"",database:"",options:{}}
+	 */
+	async open(options) {
+		this._config = Object.assign({
+			url:"mongodb://localhost:27017",
+			database:"",
+			options: {
+				socketTimeoutMS: 90000,
+				keepAlive: true,
+				poolSize: 10,
+				useUnifiedTopology: true
+			}
+		}, options || {})
+		_conns[`${this._config.url}${this._config.database}`] = this
+		this._conn = await Mongo.MongoClient.connect(this._config.url, this._config.options)
+		this._db = this._conn.db(this._config.database)
+		return this
+	}
+
+	/**
+	 * 
+	 */
+	async close(){
+		if(!this._conn)
+			return
+		try {
+			this._conn.close()
+		} catch (error) {
+			console.warn(error.message)
+		}
+	}
+}
+
+export default DataConnection
---
+++ index.cjs
@@ -0,0 +1,54 @@
+const Mongo = require("mongodb")
+
+const DataConnection = require("./dbconn.js")
+
+let _db
+let _conn
+let _options={
+        socketTimeoutMS: 90000,
+        keepAlive: true,
+        poolSize: 20,
+        useUnifiedTopology: true,
+        /*useNewUrlParser: true, */
+    
+}
+class Database {
+	static get Mongo(){
+		return Mongo
+	}
+
+	static get Client(){
+		return Mongo.MongoClient
+	}
+
+	static get Connection(){
+		return _conn
+	}
+
+	static get Database(){
+		return _db
+	}
+
+	static get ID(){
+		return Mongo.ObjectID()
+	}
+
+	static GenerateID(){
+		return Mongo.ObjectID()
+	}
+	
+	
+	static async Init(config) {
+			_conn = await Mongo.MongoClient.connect(config.url,_options)
+			_db = _conn.db(config.database)
+	}
+}
+const DataManager = new Proxy(Database, {
+	get(target, prop) {	
+		if(target[prop]===undefined)		
+			target[prop] =_db.collection(prop.toLowerCase())		
+		return target[prop]
+	}
+})
+
+module.exports = {DataManager,Database,DataConnection}
